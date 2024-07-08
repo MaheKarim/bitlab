bad6e9074655caf708f64abc65ee4557f38a869b
Mahe Karim <mahekarim@gmail.com>
2024-07-08 19:47:53 +0600

    Project Initialize :white_check_mark:

---
+++ core/app/Models/Send.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Send extends Model
+{
+    public function wallet(){
+        return $this->belongsTo(UserWallet::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
