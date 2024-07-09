3ea76b60e3ba3fab7c32e6761dc9c12a2426be22
Mahe Karim <mahekarim@gmail.com>
2024-07-09 20:09:38 +0600

    Service Pages Added

--- core/resources/views/templates/basic/sections/feature.blade.php
+++ core/resources/views/templates/basic/sections/feature.blade.php
@@ -1,10 +1,29 @@
 @php
-   /*
-    *   singleQuery True / false
-    * By default singleQuery is false , If you want only one row you have to set `True`
-    *
-    * limit to fetch record of data
-    */
-
-    #getContent('data_key','singleQuery true/false','limit');
+    $features = getContent('feature.element');
+    $feature = getContent('feature.content', true);
 @endphp
+    <!-- Feature Section -->
+<section class="feature-section pt-60 pb-120 {{ request()->routeIs('about') ? 'hero-about mt--80 pt-0' : null }}">
+    <div class="container">
+        <div class="row justify-content-center g-4">
+            @foreach($features as $feature)
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-item">
+                        <div class="icon">
+                            @php
+                                echo @$feature->data_values->icon;
+                            @endphp
+                        </div>
+                        <div class="cont">
+                            <h6 class="subtitle">{{ __(@$feature->data_values->heading) }}</h6>
+                            <p>
+                                {{ __(@$feature->data_values->sub_heading) }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
+<!-- Feature Section -->
