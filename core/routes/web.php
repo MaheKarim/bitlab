645064fac9fd08cd5c149d71e397e1f6071fd0a2
Mahe Karim <mahekarim@gmail.com>
2024-07-09 15:16:50 +0600

    Payment Gateway All Deleted :ambulance:

--- core/routes/web.php
+++ core/routes/web.php
@@ -6,8 +6,13 @@ Route::get('/clear', function(){
     \Illuminate\Support\Facades\Artisan::call('optimize:clear');
 });
 
+Route::controller('CronController')->group(function () {
+    Route::get('cron', 'cron')->name('cron');
+    Route::get('rate', 'rate')->name('cron.rate');
+    Route::get('send', 'send')->name('cron.send');
+    Route::get('receive', 'receive')->name('cron.receive');
+});
 
-Route::get('cron', 'CronController@cron')->name('cron');
 
 // User Support Ticket
 Route::controller('TicketController')->prefix('ticket')->name('ticket.')->group(function () {
@@ -24,7 +29,9 @@ Route::get('app/deposit/confirm/{hash}', 'Gateway\PaymentController@appDepositCo
 
 Route::controller('SiteController')->group(function () {
     Route::get('/contact', 'contact')->name('contact');
+
     Route::post('/contact', 'contactSubmit');
+
     Route::get('/change/{lang?}', 'changeLanguage')->name('lang');
 
     Route::get('cookie-policy', 'cookiePolicy')->name('cookie.policy');
@@ -36,8 +43,10 @@ Route::controller('SiteController')->group(function () {
     Route::get('policy/{slug}', 'policyPages')->name('policy.pages');
 
     Route::get('placeholder-image/{size}', 'placeholderImage')->withoutMiddleware('maintenance')->name('placeholder.image');
+
     Route::get('maintenance-mode','maintenance')->withoutMiddleware('maintenance')->name('maintenance');
 
     Route::get('/{slug}', 'pages')->name('pages');
+
     Route::get('/', 'index')->name('home');
 });
