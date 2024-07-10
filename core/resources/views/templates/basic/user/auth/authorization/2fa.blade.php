d27c212b56252ff9f01dbab845f90e00b6ad9195
Mahe Karim <mahekarim@gmail.com>
2024-07-10 16:58:10 +0600

    Ban Page Re Design

--- core/resources/views/templates/basic/user/auth/authorization/2fa.blade.php
+++ core/resources/views/templates/basic/user/auth/authorization/2fa.blade.php
@@ -7,9 +7,7 @@
                 <h5 class="pb-3 text-center border-bottom">@lang('2FA Verification')</h5>
                 <form action="{{route('user.2fa.verify')}}" method="POST" class="submit-form">
                     @csrf
-
                     @include($activeTemplate.'partials.verification_code')
-
                     <div class="form--group">
                         <button type="submit" class="btn btn--base w-100">@lang('Submit')</button>
                     </div>
